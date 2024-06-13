a196b34664fbbae6f1dd61799e31a79b6f144e64
Tzetgarel <abrahamzeth1@gmail.com>
2024-06-13 13:44:30 -0500

    Es la prueba definitva

---
+++ prueba.py
@@ -0,0 +1,15 @@
+# prueba.py
+
+
+
+def saludo():
+	print("Hola mundo")
+
+def suma(a, b):
+	return a + b
+
+if __name__== "__main__":
+	saludo()
+	resultado = suma(3, 5)
+	print(f"La suma de 3 y 5 es:
+ {resultado}") 
