609a5d955ab041a410f01f8d0126f15ac5d7e66d
Abraham <abrahamzeth1@gmail.com>
2024-06-25 16:59:31 -0500

    Actualizacion automatica de cambios en prueba.py

--- prueba.py
+++ prueba.py
@@ -1,15 +1,23 @@
 # prueba.py
 
- 
+
 
 def saludo():
 	print("Hola mundo")
 
+
 def suma(a, b):
 	return a + b
+
 
 if __name__== "__main__":
 	saludo()
 	resultado = suma(3, 5)
 	print(f"La suma de 3 y 5 es:
- {resultado}") 
+ {resultado}")
+
+
+AQUI HASTA ABAJO ESTAN TODOS LOS CAMBIOS NUEVOS
+
+PRUEBA V2
+ 
